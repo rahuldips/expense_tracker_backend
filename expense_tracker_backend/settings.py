fbc835698d4659f89048955116c5263ad049ea10
giri-www <girirahul944@gmail.com>
2024-04-21 20:01:04 +0530

    user auth app and db setting setup added

--- expense_tracker_backend/settings.py
+++ expense_tracker_backend/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
+    'user',
+    'authentication',
 ]
 
 MIDDLEWARE = [
@@ -73,13 +77,24 @@ WSGI_APPLICATION = 'expense_tracker_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'expense_tracker',
+        'HOST': 'localhost',
+        'PORT':  3306,
+        'USER': 'root',
+        'PASSWORD': 'admin'
     }
-}
 
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
